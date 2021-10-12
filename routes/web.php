7a38dcd1c22ac9f482eb21a8bd1d1e3575d282b5
Lucas Garcia <lucks.garcialg@gmail.com>
2021-10-12 20:18:34 -0300

    Aula 40

--- routes/web.php
+++ routes/web.php
@@ -26,13 +26,7 @@ Route::prefix('/app')->group(function () {
     Route::get('/produtos', 'ProdutosController@produtos')->name('app.produtos');
 });
 
-Route::get('/rota1', function () {
-    echo 'rota 1';
-})->name('site.rota1');
-
-Route::get('/rota2', function () {
-    return redirect()->route('site.rota1');
-})->name('site.rota2');
+Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('teste');
 
 Route::fallback(function () {
     echo 'A rota acessada não existe. <a href="' . route('site.index') . '">Clique aqui</a> para ir á pagina inicial';
