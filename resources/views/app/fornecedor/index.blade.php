3fbbec1942d75bf9e97ce54d35f442d14fe3e77d
Lucas Garcia <lucks.garcialg@gmail.com>
2021-10-14 22:38:31 -0300

    Aula 45

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -6,3 +6,15 @@
 $variavelA = 'teste';
 echo $variavelA;
 @endphp
+
+{{-- @dd($fornecedores) --}}
+
+@if (count($fornecedores) > 0 && count($fornecedores) < 10)
+    <h3>Existem alguns fornecedores cadastrados</h3>
+
+@elseif(count($fornecedores) > 10)
+    <h3>Existem vários fornecedores cadastrados</h3>
+
+@else
+    <h3>Ainda não existem fornecedores cadastrados</h3>
+@endif
