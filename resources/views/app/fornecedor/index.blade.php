79fb79039dbc14d1434e02550107d05552a9669b
Lucas Garcia <lucks.garcialg@gmail.com>
2021-10-14 22:52:18 -0300

    Aula 46

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -2,14 +2,9 @@
 
 {{-- Comentário invisível --}}
 
-@php
-$variavelA = 'teste';
-echo $variavelA;
-@endphp
-
 {{-- @dd($fornecedores) --}}
 
-@if (count($fornecedores) > 0 && count($fornecedores) < 10)
+{{-- @if (count($fornecedores) > 0 && count($fornecedores) < 10)
     <h3>Existem alguns fornecedores cadastrados</h3>
 
 @elseif(count($fornecedores) > 10)
@@ -17,4 +12,23 @@ echo $variavelA;
 
 @else
     <h3>Ainda não existem fornecedores cadastrados</h3>
-@endif
+@endif --}}
+
+{{-- @if Executa se o retorno for True --}}
+
+{{-- @unless Executa se o retorno for False --}}
+
+Fornecedor: {{ $fornecedores[0]['nome'] }}
+<br>
+Status: {{ $fornecedores[0]['status'] }}
+<br>
+
+{{-- Executa se for true --}}
+@if($fornecedores[0]['status'] == 'S')
+Fornecedor ativo
+@endif 
+
+{{-- Executa se for false --}}
+@unless($fornecedores[0]['status'] == 'S') 
+Fornecedor inativo
+@endunless
