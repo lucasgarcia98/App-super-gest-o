116bf06c8fde9ed813b3279cd843561de2b4d6f8
Lucas Garcia <lucks.garcialg@gmail.com>
2021-10-23 17:47:57 -0300

    Aula 66

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -6,7 +6,11 @@ use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contato() {
+    public function contato()
+    {
+
+        var_dump($_POST);
+
         return view('site.contato', ['titulo' => 'Contato (teste)']);
     }
 }
--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -11,21 +11,22 @@
 
         <div class="informacao-pagina">
             <div class="contato-principal">
-                <form>
-                    <input type="text" placeholder="Nome" class="borda-preta">
+                <form action={{ route('site.contato' )}} method="post">
+                    @csrf
+                    <input type="text" placeholder="Nome" class="borda-preta" name="nome">
                     <br>
-                    <input type="text" placeholder="Telefone" class="borda-preta">
+                    <input type="text" placeholder="Telefone" class="borda-preta" name="telefone">
                     <br>
-                    <input type="text" placeholder="E-mail" class="borda-preta">
+                    <input type="text" placeholder="E-mail" class="borda-preta" name="email">
                     <br>
-                    <select class="borda-preta">
+                    <select class="borda-preta" name="motivo-contato">
                         <option value="">Qual o motivo do contato?</option>
-                        <option value="">Dúvida</option>
-                        <option value="">Elogio</option>
-                        <option value="">Reclamação</option>
+                        <option value="1">Dúvida</option>
+                        <option value="2">Elogio</option>
+                        <option value="3">Reclamação</option>
                     </select>
                     <br>
-                    <textarea class="borda-preta">Preencha aqui a sua mensagem</textarea>
+                    <textarea class="borda-preta" name="mensagem">Preencha aqui a sua mensagem</textarea>
                     <br>
                     <button type="submit" class="borda-preta">ENVIAR</button>
                 </form>
