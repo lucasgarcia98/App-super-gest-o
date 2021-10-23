22605fb2c4a85547380ebfa6f627d412b39a3a06
Lucas Garcia <lucks.garcialg@gmail.com>
2021-10-23 18:10:52 -0300

    Aula 69

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -11,25 +11,10 @@
 
         <div class="informacao-pagina">
             <div class="contato-principal">
-                <form action={{ route('site.contato' )}} method="post">
-                    @csrf
-                    <input type="text" placeholder="Nome" class="borda-preta" name="nome">
-                    <br>
-                    <input type="text" placeholder="Telefone" class="borda-preta" name="telefone">
-                    <br>
-                    <input type="text" placeholder="E-mail" class="borda-preta" name="email">
-                    <br>
-                    <select class="borda-preta" name="motivo-contato">
-                        <option value="">Qual o motivo do contato?</option>
-                        <option value="1">Dúvida</option>
-                        <option value="2">Elogio</option>
-                        <option value="3">Reclamação</option>
-                    </select>
-                    <br>
-                    <textarea class="borda-preta" name="mensagem">Preencha aqui a sua mensagem</textarea>
-                    <br>
-                    <button type="submit" class="borda-preta">ENVIAR</button>
-                </form>
+                @component('site.layouts._components.form_contato', ['classe' => 'borda-preta'])
+                <p>A nossa equipe analisará sua mensagem.</p>
+                <p>Responderemos em até 48 horas.</p>
+                @endcomponent
             </div>
         </div>
     </div>
